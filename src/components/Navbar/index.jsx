a512db860edcf91ef3c414b1fba9c4610ebaf1d3
neha yadav <nehayadav23456@gmail.com>
2024-10-28 13:38:03 +0530

    changes in navbar,tab and index.css file

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,57 +2,6 @@ import React from "react";
 function Navbar() {
   return (
     <div className="navbar-container">
-      {/* <nav
-        className="navbar navbar-expand-lg bg-body-tertiary"
-        style={{ height: "5rem" }}
-      >
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <div>
-              <img
-                className="navbar-logo-image"
-                src="WhatsApp_Image_2024-09-20_at_15.31.32_d5c1b748-removebg-preview.png"
-                alt="No logo found"
-              />
-            </div>
-            <div>
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Contact
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Careers
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav> */}
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button
           class="navbar-toggler"
@@ -76,7 +25,7 @@ function Navbar() {
                 alt="No logo found"
               />
             </div>
-            <div >
+            <div>
               <div class="navbar-nav ">
                 <div class="nav-item active">
                   <a class="nav-link" href="#">
--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,64 +1,23 @@
-// import React from "react";
-// import { Tabs } from "antd";
-// import Offline from "../Offline";
-// import Online from "../Online";
-// import ChallanDekhoComponent from "../ChallanDekho";
-
-// const TabComponent = () => {
-//   const items = [
-//     {
-//       key: "1",
-//       label: "Challan Dekho",
-//       children: <ChallanDekhoComponent />,
-//     },
-//     {
-//       key: "2",
-//       label: "Online",
-//       children: <Online />,
-//     },
-//     {
-//       key: "3",
-//       label: "Offline",
-//       children: <Offline />,
-//     },
-//   ];
-//   const onChange = (key) => {
-//     console.log(key);
-//   };
-//   return (
-//     <div>
-//       <div className="pay-challan">
-//         <div className="h2-heading-pay-challan">
-//           <h2 className="how-can-pay-challan">How Can I Pay Challan</h2>
-//         </div>
-//         <div className="paragraph-pay-online-offline">
-//           <p>
-//             Electronic fines replace traditional paper challans and are directly
-//             sent to the violator's mobile device or email.However, there are
-//             three ways to pay your challan: online or offline.
-//           </p>
-//         </div>
-//       </div>
-
-//       <Tabs
-//         defaultActiveKey="1"
-//         items={items}
-//         onChange={onChange}
-
-//       />
-//     </div>
-//   );
-// };
-
-// export default TabComponent;
-
 import React from "react";
 import Offline from "../Offline";
 import Online from "../Online";
 import ChallanDekhoComponent from "../ChallanDekho";
 const TabComponent = () => {
   return (
-    <>
+    <div>
+      <div className="pay-challan">
+        <div className="h2-heading-pay-challan">
+          <h2 className="how-can-pay-challan">How Can I Pay Challan</h2>
+        </div>
+        <div className="paragraph-pay-online-offline">
+          <p>
+            Electronic fines replace traditional paper challans and are directly
+            sent to the violator's mobile device or email.However, there are
+            three ways to pay your challan: online or offline.
+          </p>
+        </div>
+      </div>
+
       <ul class="nav nav-tabs" role="tablist">
         <li class="nav-item-tab-0" role="presentation">
           <a
@@ -132,7 +91,7 @@ const TabComponent = () => {
           <Offline />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
